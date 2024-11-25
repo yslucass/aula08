e25628488a11ced4c9634903ec02744eeda7aaa4
yslucass <lucas.suguimoto@estudante.ifms.edu.br>
2024-11-25 17:40:06 -0400

    aaa

--- cliente/src/pages/Registro.jsx
+++ cliente/src/pages/Registro.jsx
@@ -1,9 +1,30 @@
 import { useState } from "react";
 
 export default function Registrar() {
+    const [Nome, setNome] = useState("");
+    const [Email, setEmail] = useState("");
 
+    const registro = async () => {
+      try{
+          await fetch('https//localhost:3000/usuarios', {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            Nome: Nome,
+            Email: Email
+          })
+        });
+      } catch{
+        alert("Ocorreu um ero na aplicação!");
+      }
+    }
 
   return (
-        
+        <main>
+          <form onSubmit={event.preventDefault}>
+              <input type="text" value={Nome} onChange={(event) => setNome(event.target.value)}></input>
+              <input type="text" value={Nome} onChange={(event) => setEmail(event.target.value)}></input>
+          </form>
+        </main>
   );
 }
